09d883c51a0a82b60974ed3e6d7ae9f13a7e353e
Thiago Miranda de Souza <Thiago.msouza0@gmail.com>
2018-04-23 12:12:01 -0300

    calc

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -23,3 +23,70 @@ multiplicação (x), então no input deve aparecer "1+2x".
 input;
 - Ao pressionar o botão "CE", o input deve ficar zerado.
 */
+(function(win,doc){
+'use strict';
+    var $visor  = doc.querySelector('[data-js="Display"]');
+    var $botaonumero = doc.querySelectorAll('[data-js="button-number"]');
+    var $botaoCE = doc.querySelector('[data-js="button-opetation-CE"]'); 
+    var $botaoOpercao = doc.querySelectorAll('[data-js="button-opetation"]');
+    var $botaoigual = doc.querySelector('[data-js="button-opetation-="]')
+ 
+    Array.prototype.forEach.call($botaonumero,function(botaoNumer){
+      botaoNumer.addEventListener("click",aoclicarBotaoNumero,false) 
+    })
+
+    Array.prototype.forEach.call($botaoOpercao,function(botaoOperacao){
+      botaoOperacao.addEventListener("click",cliqueOperacao,false)
+    })
+
+    function cliqueOperacao(){
+      $visor.value = verificaUltimoDigito($visor.value);
+      $visor.value += this.value;
+    }
+
+    $botaoCE.addEventListener("click",aoClicarCE,false);
+    $botaoigual.addEventListener("click",seraIgual,false);
+
+    function aoclicarBotaoNumero(){
+      if ($visor.value === "0" )
+        $visor.value="" ;
+      $visor.value += this.value;      
+    };
+
+    function aoClicarCE(){
+      $visor.value = 0;
+    };
+
+    function isOperador(digito){
+      var operadores = ['+','-','*','÷']
+      var ultimoItem = digito .split('').pop();  
+      return operadores.some(function(operador){
+        return operador === ultimoItem;
+      }) 
+    };
+
+    function verificaUltimoDigito(numero){
+      if (isOperador(numero))
+        return numero.slice(0,-1)
+      return numero;
+    };
+
+    function seraIgual(){
+      $visor.value = verificaUltimoDigito($visor.value);
+      var allValue = $visor.value.match(/\d+[+*÷-]?/g);
+      $visor.value = allValue.reduce(function(acumulado,atual){
+        var primeiroValor = acumulado.slice(0,-1);
+        var operador = acumulado.split('').pop();
+        var ultimoValor = verificaUltimoDigito(atual);
+        var ultimoSinal = isOperador(atual) ? atual.split('').pop() : '';
+          switch(operador){
+            case'+':return  (Number(primeiroValor) + Number(ultimoValor)) + ultimoSinal ;
+            case'-':return  (Number(primeiroValor) - Number(ultimoValor)) + ultimoSinal ; 
+            case'*':return  (primeiroValor  * ultimoValor) + ultimoSinal ;
+            case'÷':return  (primeiroValor / ultimoValor) + ultimoSinal ;
+          };
+      })
+      $visor.value = result;
+    ;}
+
+})(window,document);
